eaf6d1f1d486e0a7186e369268c783e49551d60b
Mike Lofgren <mikeylofgren@gmail.com>
2020-07-03 12:01:11 -0700

    pipe in different filters.

--- directory.js
+++ directory.js
@@ -1,16 +1,17 @@
 angular
   .module('directoryApp', [])
-  .controller('directoryController', ($scope) => {
-    $scope.list = [
+  .controller('directoryController', function () {
+    const dirList = this;
+    dirList.list = [
       { name: 'Mike', age: 5999 },
       { name: 'Pep', age: 9 },
       { name: 'Ron', age: 5 },
       { name: 'Don', age: 4 },
     ];
 
-    $scope.addPerson = () => {
-      $scope.list.push({ name: $scope.name, age: $scope.age });
-      $scope.name = '';
-      $scope.age = 0;
+    dirList.addPerson = () => {
+      dirList.list.push({ name: dirList.name, age: dirList.age });
+      dirList.name = '';
+      dirList.age = 0;
     };
   });
